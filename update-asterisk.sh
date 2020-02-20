1e56d00a0c055915a974c4cbf1206b00d37599f9
root <root@anthonyd973.ddns.net>
2020-02-20 01:22:18 +0000

    Updated the update script.

--- update-asterisk.sh
+++ update-asterisk.sh
@@ -4,14 +4,19 @@ THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 GIT_ROOT_DIR="${THIS_SCRIPT_DIR}"
 ASTERISK_DIR="${GIT_ROOT_DIR}/asterisk"
 INSTALL_ASTERISK_DIR="/etc/asterisk"
+DATA_DIR="${GIT_ROOT_DIR}/data"
+MOH_DIR="${DATA_DIR}/music-on-hold"
+INSTALL_MOH_DIR="/usr/share/asterisk/moh"
 
 if [ "$(whoami)" != root ]; then
     echo Must be root >&2
     exit 1
 fi
 
-rsync -Pruz "${ASTERISK_DIR}"/* "${INSTALL_ASTERISK_DIR}"
+rsync -Pruz --delete-after "${ASTERISK_DIR}/" "${INSTALL_ASTERISK_DIR}"
+rsync -Pruz --delete-after "${MOH_DIR}/" "${INSTALL_MOH_DIR}"
 chown -R asterisk "${INSTALL_ASTERISK_DIR}"
 chgrp -R asterisk "${INSTALL_ASTERISK_DIR}"
+chmod -R 640 "${INSTALL_ASTERISK_DIR}"
 
-echo $'core reload\nexit' | asterisk -vvvvvvr
+asterisk -rx 'core reload'
