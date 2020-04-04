76c11d3415ae41a4f1644a71daf8fc71c21ad207
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2020-04-03 22:10:11 -0400

    Implemented docker container creation. Does not execute well yet, though.

--- update.sh
+++ update.sh
@@ -10,28 +10,87 @@ source "${COMMON_SCRIPT}"
 # Parse other scripts.
 source "${SCRIPTS_DIR}/ami-client"
 source "${SCRIPTS_DIR}/asterisk"
+source "${SCRIPTS_DIR}/container"
 source "${SCRIPTS_DIR}/moh"
 source "${SCRIPTS_DIR}/systemd"
 source "${SCRIPTS_DIR}/templates"
 
 usage() {
-    echo "Usage:" >&2
+    echo "USAGE" >&2
     echo "${THIS_SCRIPT_BASENAME} (-h | --help)" >&2
     echo "" >&2
-    echo "${THIS_SCRIPT_BASENAME} (-c | --conf) <conf_file>" >&2
+    echo "${THIS_SCRIPT_BASENAME} container (-c | --conf) <file>" >&2
+    echo "" >&2
+    echo "${THIS_SCRIPT_BASENAME} container-init (-c | --conf) <file>" >&2
+    echo "" >&2
+    echo "${THIS_SCRIPT_BASENAME} install (-c | --conf) <file>" >&2
     echo "" >&2
     echo "" >&2
-    echo "conf_file: A file where each non-empty line looks like var=value." >&2
+    echo "OPTIONS" >&2
+    echo "" >&2
+    echo "  -h | --help Prints this help message and exit." >&2
+    echo "" >&2
+    echo "container:" >&2
+    echo "  Create and start a docker container, rather than installing on system." >&2
+    echo "  -c | --conf <file> Specifies the configuration file." >&2
+    echo "" >&2
+    echo "container-init:" >&2
+    echo "  To be ran inside a container." >&2
+    echo "  -c | --conf <file> Specifies the configuration file." >&2
+    echo "" >&2
+    echo "install:" >&2
+    echo "  Install on local system." >&2
+    echo "  -c | --conf <file> Specifies the configuration file." >&2
+    echo "  -d | --docker Create and start a docker container, rather than installing" >&2
+    echo "    installing on system." >&2
+    echo "" >&2
+    echo "" >&2
+    echo "NOTES" >&2
+    echo "" >&2
+    echo "The configuration file should be a file where each non-empty" >&2
+    echo "line looks like var=value." >&2
+    echo "Required template variables:" >&2
+    for templateVar in "${TEMPLATE_VARS[@]}"; do
+        echo "${templateVar}" >&2
+    done
 }
 
 parseArgs() {
-    if [ $# -gt 0 ]; then
-        local arg="${1}"
-        case "${arg}" in
-        --help | -h)
-            usage
-            exit 0
-            ;;
+    if [ $# -lt 1 ]; then
+        usage
+        exit 1
+    fi
+
+    local arg="${1}"
+    case "${arg}" in
+    --help | -h)
+        usage
+        exit 0
+        ;;
+    container)
+        shift
+        parseContainerArgs "$@"
+        ;;
+    container-init)
+        shift
+        parseContainerInitArgs "$@"
+        ;;
+    install)
+        shift
+        parseInstallArgs "$@"
+        ;;
+    *)
+        messageError "Unknown argument \"${arg}\"."
+        usage
+        exit 1
+        ;;
+    esac
+}
+
+parseContainerArgs() {
+    MODE=container
+    while [ $# -gt 0 ]; do
+        case "${1}" in
         --conf | -c)
             if [ $# -lt 2 ]; then
                 messageError "Missing argument after \"${arg}\"."
@@ -39,14 +98,65 @@ parseArgs() {
                 exit 1
             fi
             VAR_CONFIG_FILE="${2}"
+            shift 2
             ;;
         *)
             messageError "Unknown argument \"${arg}\"."
-            usage
-            exit 1
             ;;
         esac
+    done
+
+    if [ "${VAR_CONFIG_FILE}" == "" ]; then
+        messageError "--conf argument missing."
+        usage
+        exit 1
     fi
+}
+
+parseContainerInitArgs() {
+    MODE=container-init
+    while [ $# -gt 0 ]; do
+        case "${1}" in
+        --conf | -c)
+            if [ $# -lt 2 ]; then
+                messageError "Missing argument after \"${arg}\"."
+                usage
+                exit 1
+            fi
+            VAR_CONFIG_FILE="${2}"
+            shift 2
+            ;;
+        *)
+            messageError "Unknown argument \"${arg}\"."
+            ;;
+        esac
+    done
+
+    if [ "${VAR_CONFIG_FILE}" == "" ]; then
+        messageError "--conf argument missing."
+        usage
+        exit 1
+    fi
+}
+
+parseInstallArgs() {
+    MODE=install
+    while [ $# -gt 0 ]; do
+        case "${1}" in
+        --conf | -c)
+            if [ $# -lt 2 ]; then
+                messageError "Missing argument after \"${arg}\"."
+                usage
+                exit 1
+            fi
+            VAR_CONFIG_FILE="${2}"
+            shift 2
+            ;;
+        *)
+            messageError "Unknown argument \"${arg}\"."
+            ;;
+        esac
+    done
 
     if [ "${VAR_CONFIG_FILE}" == "" ]; then
         messageError "--conf argument missing."
@@ -69,16 +179,48 @@ cleanupTrapHandler() {
     cleanTemplateVariables
 }
 
-run() {
-    parseArgs "$@"
-    assertRoot
+install() {
     setupCleanupTrap
     parseVariableConfigFile
     updateAsteriskConfig
     updateMusicOnHold
     updateAmiClient
+}
+
+runContainerMode() {
+    setupCleanupTrap
+    parseVariableConfigFile
+    buildContainer
+    startContainer
+}
+
+runContainerInitMode() {
+    install
+}
+
+runInstallMode() {
+    install
     updateSystemd
     restartAsterisk
 }
 
-run "$@"
+main() {
+    parseArgs "$@"
+
+    case "${MODE}" in
+    container)
+        runContainerMode
+        ;;
+    container-init)
+        runContainerInitMode
+        ;;
+    install)
+        runInstallMode
+        ;;
+    *)
+        echo "Bug in script: Unknown mode ${MODE}"
+        ;;
+    esac
+}
+
+main "$@"
